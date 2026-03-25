778161438a58cffe5983b2412a62454be1d5847d
jordi-josc <josc@odoo.com>
2026-03-25 01:32:52 +0000

    hace el post correctamente

--- discord_connector/models/sale_order.py
+++ discord_connector/models/sale_order.py
@@ -1,11 +1,26 @@
 from odoo import fields, models
+import requests
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     
     discord_rule_id = fields.Many2one('discord.rule')
     
+    discord_channel_id = fields.Many2one('discord.channel')
+    
     def action_confirm(self):
-        super().action_confirm()
+        link = self.env['ir.config_parameter'].sudo().get_param('discord.sales_channel')
         
-        return self.discord_rule_id.event == 'sale_confirmed'
+        response = requests.post(link, json={"content": f"✅ Pedido confirmado: {self.name}"},)
+        
+        if response.status_code == 204:
+            _logger.info("Mensaje enviado con éxito")
+        else:
+            _logger.info(f"Error al enviar: {response.status_code}, {response.text}")
+            
+
+        
+        return super().action_confirm()
