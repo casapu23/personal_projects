d2a02e6fca43f413c0c3a29fec61a474286decd6
casapu23 <jordisc369@gmail.com>
2026-08-25 19:09:00 +0000

    [ADD][1] working_schedule_periods: first commit

---
+++ working_schedule_periods/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Working schedule periods",
+    'version': "19.0.1.0.0",
+    'author': "casapu23",
+    'license': "AGPL-3",
+    'website': "https://github.com/casapu23/personal_projects",
+    'category': "Employee",
+    'depends': ["hr"],
+    'data': [
+
+    ],
+}
